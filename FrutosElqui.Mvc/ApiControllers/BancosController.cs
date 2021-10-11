74ae8c39a8086c858261cc9d91854b8790f853d7
Felipe Alvarez Ortiz <me@felipealvarez.dev>
2021-10-11 20:41:08 -0300

    - Prueba de controladores API

---
+++ FrutosElqui.Mvc/ApiControllers/BancosController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FrutosElqui.Core.Misc;
+using FrutosElqui.Mvc.Controllers;
+using FrutosElqui.Negocio.Misc.Bancos;
+
+namespace FrutosElqui.Mvc.ApiControllers
+{
+    public class BancosController : BaseController
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Banco>>> ListaBanco()
+        {
+            return await Mediator.Send(new ListaDeBancos.Query());
+        }
+    }
+}
