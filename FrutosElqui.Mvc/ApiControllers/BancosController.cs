16bd4bd15058d6aff92e46750c500986d9168de1
falvarezortiz <falvarezortiz@hotmail.com>
2021-10-11 21:15:47 -0300

    - Se crea el controller para los bancos. - Se mueve la plantilla mediatR a la carpeta api controllers

--- FrutosElqui.Mvc/ApiControllers/BancosController.cs
+++ FrutosElqui.Mvc/ApiControllers/BancosController.cs
@@ -4,15 +4,36 @@ using System.Threading.Tasks;
 using FrutosElqui.Core.Misc;
 using FrutosElqui.Mvc.Controllers;
 using FrutosElqui.Negocio.Misc.Bancos;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 
 namespace FrutosElqui.Mvc.ApiControllers
 {
+    [Authorize]
     public class BancosController : BaseController
     {
         [HttpGet]
         public async Task<ActionResult<List<Banco>>> ListaBanco()
         {
             return await Mediator.Send(new ListaDeBancos.Query());
+        }
+
+        [HttpPost,Authorize(Roles = "Administrador")]
+        public async Task<ActionResult<Unit>> CrearBanco(CrearBanco.Command request)
+        {
+            return await Mediator.Send(request);
+        }
+
+        [HttpPost,Authorize(Roles = "Administrador")]
+        public async Task<ActionResult<Unit>> ActualizarBanco(ActualizarBanco.Command request)
+        {
+            return await Mediator.Send(request);
+        }
+
+        [HttpGet("{idBanco:int}")]
+        public async Task<ActionResult<Banco>> ObtenerBanco(int idBanco)
+        {
+            return await Mediator.Send(new ObtenerBanco.Query{IdBanco = idBanco});
         }
     }
 }
